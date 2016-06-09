c6165792cbe50cb534a9bb8f215c3c4d7c4739b1
David Liu <rumalzliu@gmail.com>
2016-06-08 22:03:49 -0400

    Initial commit

---
+++ Python/Intro/printFunction.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+
+a = int(raw_input())
+for i in range (1,a+1):
+    print(i,sep='', end='')
---
+++ Python/Intro/whatsYourName.py
@@ -0,0 +1,3 @@
+a= raw_input()
+b= raw_input()
+print 'Hello ' + a + ' ' + b + '! You just delved into python.'
